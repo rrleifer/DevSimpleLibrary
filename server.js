9e22ecfa0caf6c246eeabc827363612f12952cb5
Leonid Leifer <rrleifer@gmail.com>
2023-06-10 22:07:53 -0400

    Initial commit for authors routes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ if (process.env.NODE_ENV !== 'production') {
   const express = require('express')
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
-//   const bodyParser = require('body-parser')
+  const bodyParser = require('body-parser')
 //   const methodOverride = require('method-override')
   
   const indexRouter = require('./routes/index')
-//   const authorRouter = require('./routes/authors')
+  const authorRouter = require('./routes/authors')
 //   const bookRouter = require('./routes/books')
   app.set('view engine', 'ejs')
   app.set('views', __dirname + '/views')
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(expressLayouts)
 //   app.use(methodOverride('_method'))
   app.use(express.static('public'))
-//   app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
   
   const mongoose = require('mongoose')
   mongoose.connect(process.env.DATABASE_URL)
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV !== 'production') {
   db.once('open', () => console.log('Connected to Mongoose'))
   
   app.use('/', indexRouter)
-//   app.use('/authors', authorRouter)
+  app.use('/authors', authorRouter)
 //   app.use('/books', bookRouter)
   
   app.listen(process.env.PORT || 3000)
